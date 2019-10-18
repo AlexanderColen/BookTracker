5af3dc77da3cd5f2b917420acd347143ed161538
Alexander Colen <alexanderc@live.nl>
2019-10-17 22:18:07 -0400

    Added Book entity API handling.

---
+++ BookTracker/Controllers/BooksController.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BookTracker.Models;
+using BookTracker.Models.Contexts;
+
+namespace BookTracker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly BookContext _context;
+
+        public BooksController(BookContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Books
+        [HttpGet]
+        public IEnumerable<Book> GetBookItems()
+        {
+            return _context.BookItems;
+        }
+
+        // GET: api/Books/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetBook([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var book = await _context.BookItems.FindAsync(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(book);
+        }
+
+        // PUT: api/Books/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBook([FromRoute] int id, [FromBody] Book book)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != book.BookID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(book).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BookExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Books
+        [HttpPost]
+        public async Task<IActionResult> PostBook([FromBody] Book book)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.BookItems.Add(book);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBook", new { id = book.BookID }, book);
+        }
+
+        // DELETE: api/Books/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBook([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var book = await _context.BookItems.FindAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            _context.BookItems.Remove(book);
+            await _context.SaveChangesAsync();
+
+            return Ok(book);
+        }
+
+        private bool BookExists(int id)
+        {
+            return _context.BookItems.Any(e => e.BookID == id);
+        }
+    }
+}
---
+++ BookTracker/Models/Contexts/BookContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookTracker.Models.Contexts
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions<BookContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> BookItems { get; set; }
+    }
+}
